134c3adc1b941c7eab159f3db4a82026fa3e4c84
Bruno <brunocamposo.r@gmail.com>
2026-06-08 17:03:46 -0300

    Testando IP

--- lamport/src/main/java/com/lamport/ProcessNode.java
+++ lamport/src/main/java/com/lamport/ProcessNode.java
@@ -3,7 +3,7 @@ package com.lamport;
 public enum ProcessNode {
 
     P1(1, "192.168.0.12", 5000),
-    P2(2, "192.168.0.12", 5001),
+    P2(2, "192.168.0.2", 5001),
     P3(3, "192.168.0.149", 5000);
 
     private final int id;
